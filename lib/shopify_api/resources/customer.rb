4b82dec7b1377f88ba50a6fb88e3e734ee26450d
SeniorDevCode <senior.dev.software@gmail.com>
2014-02-19 10:26:33 -0500

    create a Gemfile for threadsafe AR

--- lib/shopify_api/resources/customer.rb
+++ lib/shopify_api/resources/customer.rb
@@ -3,11 +3,8 @@ module ShopifyAPI
     include Metafields
 
     def orders
-      Order.find(:all, params: {customer_id: self.id})
+      Order.find(:all, :params => {:customer_id => self.id})
     end
 
-    def self.search(params)
-      find(:all, from: :search, params: params)
-    end
   end
 end

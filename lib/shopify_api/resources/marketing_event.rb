f8cd8f56ca81804e9c3f7edb386257616bd08854
SeniorDevCode <senior.dev.software@gmail.com>
2016-11-20 20:40:32 -0500

    add URL parameters to metafiels

--- lib/shopify_api/resources/marketing_event.rb
+++ lib/shopify_api/resources/marketing_event.rb
@@ -1,8 +1,4 @@
 module ShopifyAPI
   class MarketingEvent < Base
-    def add_engagements(engagements)
-      engagements = { engagements: Array(engagements) }
-      post(:engagements, {}, engagements.to_json)
-    end
   end
 end
